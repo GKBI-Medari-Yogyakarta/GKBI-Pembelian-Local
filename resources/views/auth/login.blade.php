e2b3a97d3017ab5067cf84a96070794a5e68c934
WahidinAji <a17wahidin@gmail.com>
2021-01-04 10:10:17 +0700

    update login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,178 +1,40 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <style>
-        * {
-        box-sizing:border-box;
-        }
-        $font-stack: 14px Helvetica, sans-serif;
-        $primary-color: #666;
-
-        main {
-        width:50em;
-        margin:0 auto;
-        }
-
-        form {
-        z-index:999;
-        font: $font-stack;
-        color: $primary-color;
-        margin:20px auto;
-        width: 400px;
-        height: 400px;
-        border: 1px solid #cecece;
-        border-radius: 50%;
-        padding:20px;
-        display:grid;
-        box-shadow:0px 0px 4px #cecece;
-        justify-items:center;
-        position:relative;
-        background:#fff;
-        top:-27em;
-        }
-
-
-        label {
-        margin:0 0 10px 0;
-        }
-
-        input[type="text"], input[type="password"], {
-        height:45px;
-        width:100%;
-        padding:5px;
-        border:0;
-        border-bottom:1px solid #ccc;
-        outline:none;
-        }
-
-        .forgot-text,
-        a,
-        a:link,
-        a:hover,
-        a:active {
-        font:11px sans-serif;
-        color:#71def2;
-        margin:7px 0 15px 3px;
-        text-decoration:none;
-        }
-
-        a:hover {
-        transition:color .25s ease-in-out;
-        text-decoration:underline;
-        color:#3d828e;
-        }
-
-        input:focus {
-        background: #e8f9fc;
-        transition: all .15s ease-in-out;
-        }
-
-
-        .keep-logged-in {
-        width:100%;
-        text-align:center;
-        }
-
-
-        input[type="submit"] {
-        width: 70px;
-        height:70px;
-        border-radius:100px;
-        border:1px solid #3d828e;
-        background:#54b7c9;
-        margin:0 auto;
-        color: #fff;
-        text-transform:uppercase;
-        margin-top: 20px;
-        padding:0;
-        justify-items:center;
-        }
-
-        input[type="submit"]:hover {
-        background:#71def2;
-        transition:all .15s ease-in-out;
-        cursor: pointer;
-        }
-
-        input[type="submit"]:focus {
-        font-weight:bold;
-        background:#71def2;
-        width: 90px;
-        transition: all .25s ease-in;
-        }
-
-        form div {
-        width:90%;
-        margin:10px 0;
-
-        }
-        .circle {
-        width: 400px;
-        height: 400px;
-        border: 1px solid #cecece;
-        border-radius: 50%;
-        padding:20px;
-        display:grid-inline;
-        box-shadow:0px 0px 4px #cecece;
-        position:relative;
-        }
-
-        .circle-lime {
-        z-index:1;
-        background:#A8F781;
-        top:.6em;
-        left:11em;
-        filter: opacity(70%);
-        }
-
-        .circle-aqua {
-        z-index:2;
-        background:#71def2;
-        left:14.4em;
-        top:-48em;
-        filter: opacity(70%);
-        }
-    </style>
-</head>
-
-<body>
-    <main>
-        <div class="circle circle-lime"></div>
-        <form action=" {{ URL::route('post.login') }}" method="POST">
-            {{ csrf_field() }}
-            {{-- @csrf --}}
-            <h1>Login</h1>
-            <div class="form-group">
-                <label for="name"><i class="zmdi zmdi-account material-icons-name"></i></label>
-                <input type="text" name="name" id="name" placeholder="Your Name"/>
-                @if ($errors->has('name'))
-                <span class="error">
-                  {{ $errors->first('name') }}
-                </span>
-                @endif
+@extends('auth.master')
+@section('main')
+<main>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-lg-5">
+                <div class="card shadow-lg border-0 rounded-lg mt-5">
+                    <div class="card-header"><h3 class="text-center font-weight-light my-4">Login</h3></div>
+                    <div class="card-body">
+                        <form action="{{ URL::route('post.login') }}" method="POST">
+                            {{ csrf_field() }}
+                            <div class="form-group">
+                                <label class="large mb-1" for="inputEmailAddress">Nama</label>
+                                <input class="form-control py-4" id="inputEmailAddress" type="text" placeholder="Masukkan nama user" value="{{ old('name') }}" name="name"/>
+                                @if ($errors->has('name'))
+                                    <span class="error">
+                                    {{ $errors->first('name') }}
+                                    </span>
+                                @endif
+                            </div>
+                            <div class="form-group">
+                                <label class="large mb-1" for="inputPassword">Password</label>
+                                <input class="form-control py-4" id="inputPassword" type="password" placeholder="Masukkan password" value="{{ old('password') }}" name="password"/>
+                                @if ($errors->has('password'))
+                                    <span class="error">
+                                        {{ $errors->first('password') }}
+                                    </span>
+                                @endif
+                            </div>
+                            <div class="form-group d-flex align-items-center justify-content-between mt-4 mb-0">
+                                <button type="submit" class="btn btn-primary">Login</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
             </div>
-            <div class="form-group">
-                <label for="inputPassword"><i class="zmdi zmdi-lock"></i></label>
-                <input type="password" name="password" id="inputPassword" placeholder="Password"/>
-                @if ($errors->has('password'))
-                  <span class="error">
-                    {{ $errors->first('password') }}
-                  </span>
-                @endif
-            </div>
-            <div class="keep-logged-in">
-                <input type="checkbox" value="Go!"></input>
-                <label>Keep me logged in.</label>
-            </div>
-            <input type="submit" value="Go!"></input>
-        </form>
-        <div class="circle circle-aqua"></div>
-    </main>
-</body>
-
-</html>
+        </div>
+    </div>
+</main>
+@endsection
