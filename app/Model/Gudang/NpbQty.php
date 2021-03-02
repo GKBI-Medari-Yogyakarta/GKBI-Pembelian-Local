324867b5e8db273fd0743447f23fdea661c1d9af
WahidinAji <a17wahidin@gmail.com>
2021-03-02 13:09:28 +0700

    add mess assignment for npbqty

--- app/Model/Gudang/NpbQty.php
+++ app/Model/Gudang/NpbQty.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class NpbQty extends Model
 {
-    //
+    protected $fillable = ['ti_id', 'no_urut'];
+    public $timestamps = true;
+    public function testingItem()
+    {
+        return $this->belongsTo(TestingItem::class, 'ti_id');
+    }
 }
